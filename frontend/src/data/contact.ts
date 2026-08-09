cde78edcc92b758549e53feb9aa75e92bef9983b
tanoyronsaha381 <tanoyronsaha381@gmail.com>
2026-08-10 00:47:43 +0530

    Update clinic phone number

--- frontend/src/data/contact.ts
+++ frontend/src/data/contact.ts
@@ -1,5 +1,5 @@
-export const clinicPhone = '6297442142';
-export const clinicPhoneDisplay = '+91 62974 42142';
+export const clinicPhone = '8981561861';
+export const clinicPhoneDisplay = '+91 89815 61861';
 export const clinicPhoneHref = `tel:+91${clinicPhone}`;
 
 export const clinicEmail = 'care@orthoraja.com';
